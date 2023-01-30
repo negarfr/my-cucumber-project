fc63cffc6a7c54c413fb6940a69c4f2730aa0173
Negar France <negar.taajbakhsh@mail.com>
2023-01-30 10:26:41 +0000

    message

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -3,10 +3,8 @@ package runners;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class) // this annotation will run our class
+@RunWith(Cucumber.class)
 @CucumberOptions(
-
         plugin = {
                 "pretty",
                 "html:target/default-cucumber-reports.html",
@@ -15,19 +13,21 @@ import org.junit.runner.RunWith;
                 "rerun:target/failedRerun.txt",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         },
-
-        monochrome = false,
-        features = "./src/test/resources/features",//PATH OF FEATURES FOLDER
-        glue = {"stepdefinitions","hooks"}, //PATH OF STEP DEFINITIONS
+        monochrome=true,
+        features = "./src/test/resources/features",
+        glue = {"stepdefinitions", "hooks"},
         dryRun = false,
-        tags = "@parametrizing1"
+        tags = "@failed_tests"
 )
+
+
+
 public class Runner {
 
 }
 /*
  features = "./src/test/resources/features",//PATH OF FEATURES FOLDER
-         glue = "stepdefinitions", //PATH OF STEP DEFINITIONS
-         dryRun = false //IF dryRun = true. RUN DRY. JUST GIVE ME THE MISSING STEP DEFINITIONS. DON'T RUN THE TEST CASES.
-         dryRun= false when we are doing test execution. dryRun=true when we are generation missing step definitions
-         */
+ glue = "stepdefinitions", //PATH OF STEP DEFINITIONS
+ dryRun = false //IF dryRun = true. RUN DRY. JUST GIVE ME THE MISSING STEP DEFINITIONS. DON'T RUN THE TEST CASES.
+ dryRun= false when we are doing test execution. dryRun=true when we are generation missing step definitions
+*/
